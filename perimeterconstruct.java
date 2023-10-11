1de27610ca4be3ee14fa11e0bd26a83381d5c9ad
Harmless04 <119772389+Harmless04@users.noreply.github.com>
2023-10-11 14:28:37 +0530

    a java program for finding the perimeter
    perimeter of two rectangle using construction, object oriented programme, javalab , 11/10/2023

---
+++ perimeterconstruct.java
@@ -0,0 +1,39 @@
+import java.util.*;
+	class perimeterconstruct
+	{
+		int length,breadth,p;
+		perimeterconstruct()
+		{
+			length=10;
+			breadth=20;
+		}
+
+perimeterconstruct(int l,int b)
+	{
+		length=l;
+		breadth=b;
+	}
+void calculation()
+{
+	p= 2*(length+breadth);
+	System.out.println(" "+p);
+}
+	public class Main
+	{	
+		public static void main(String args[])
+		
+
+		{
+			Scanner in= new Scanner (System.in);
+			Int l,b
+			l =in.next int
+			b =in.next int
+			rect obj1 =new rect();
+			rect obj2 =new rect();
+		obj1.perimeterconstruct();
+		obj2.perimeterconstruct(l,b);
+	}
+}
+
+
+	
